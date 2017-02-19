6870a246d1a04dc41c709e468c69835c5b9630ee
Peter Mankiewich <pmank64@gmail.com>
2017-02-18 19:17:36 -0500

    Initial commit

--- static/js/scripting.js
+++ static/js/scripting.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
     
 	var width = $(window).width();
 
-	if (width > 766){
+	if (width > 766) {
 
 		var height = $(window).height();
 		$(".col-height").css("height",  height + "px");
